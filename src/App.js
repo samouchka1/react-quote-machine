760576a6f4c67e080660feccfceb3fba8dc98b84
samouchka1 <mchio421@protonmail.com>
2022-09-05 17:46:07 -0400

    first draft

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import React, { useState } from 'react';
 import './App.css';
+import {
+  Container,
+  Box,
+  Typography,
+  Button,
+} from '@mui/material';
+
+const containerStyles = {
+  width: {
+    xs: 'xs',
+    md: 'md'
+  },
+  margin: '10rem auto',
+}
 
 function App() {
+
+ const [quote, setQuote] = useState('');
+ const [author, setAuthor] = useState('');
+
+
+ const getQuotes = () => {
+  const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+  
+  fetch(url).then((res) => res.json()).then((data) => {
+    let dataQuotes =  data.quotes;
+
+    let randomNum = Math.floor(Math.random() * dataQuotes.length);
+    let randomQuote = data.quotes[randomNum];
+
+    setQuote(randomQuote.quote);
+    setAuthor(randomQuote.author);
+  })
+
+ }
+
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container maxWidth='md' sx={containerStyles}>
+      <Box sx={{width: '100%', textAlign: 'center'}}>
+        <Typography sx={{padding: '2rem 0'}}>{quote}</Typography>
+        <Typography sx={{paddingBottom: '2rem'}}> - {author}</Typography>
+        <Button onClick={getQuotes}>Quote</Button>
+      </Box>
+    </Container>
   );
 }
 
